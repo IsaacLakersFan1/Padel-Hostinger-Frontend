a3634cdafdd8fa7a85813d59344bc4db06d7e379
Isaac Salazar <isaacsalazar9911@gmail.com>
2025-02-19 01:05:09 -0600

    Feat: add new backend to prod

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -1,3 +1,4 @@
-const API_URL = 'http://localhost:3000'
+// const API_URL = 'http://localhost:3000'
+const API_URL = 'http://m4kcgo0c0sw0kgos4wc04ssg.193.46.198.43.sslip.io'
 
 export default API_URL;
